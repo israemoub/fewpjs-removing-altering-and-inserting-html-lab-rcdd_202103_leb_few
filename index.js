86ed25107f7895257c3b8ab103e5af3937b1dd63
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 08:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 let main = document.getElementById('main');
 main.remove();
 
+<<<<<<< HEAD
  let newHeader = document.createElement('h1');
  newHeader.setAttribute('id','victory');
 
  document.body.appendChild(newHeader);
  
  newHeader.innerHTML = "Israe is the champion";
+=======
+let newHeader = createElement('h1');
+newHeader.setAttribute(‘id’,‘victory’);
+
+document.body.appendChild(newHeader);
+>>>>>>> 68869ab8990d409862414f426e3a71cb1f515055
